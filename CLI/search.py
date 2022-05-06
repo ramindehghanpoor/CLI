79eb82dcf804ec401751cbe55eb6cf99f4f46fa9
CJ Fogel <fogelcj@gmail.com>
2022-05-06 00:19:58 -0400

    fix -ns

--- CLI/search.py
+++ CLI/search.py
@@ -7,6 +7,7 @@ import argparse
 import numpy as np
 import glob
 import sys
+from pathlib import Path
 from .utils.io import read_fasta
 from keras.models import model_from_json
 from .utils import aa_letters
@@ -42,7 +43,12 @@ class SearchOutput:
             else:
                 outf.write(self.ls + ',' + self.distance_metric + ',' + self.closest + ',' + self.distance + '\n')
 
-def new_sequence(ns):    
+def new_sequence(ns):
+
+    #
+    # Currently needs to be run from folder with Trained_networks directory in it
+    #
+    
     protein_seq_file = open(ns,"r+")
     protein_seq = protein_seq_file.read()
     
@@ -70,52 +76,56 @@ def new_sequence(ns):
         if int(seq_lengths['size'][i]) < len(test_seq):
             continue
 
-        # load the trained network
-        json_file = open('Trained_networks/' + seq_lengths['name'][i] + '.json', 'r')
-        loaded_model_json = json_file.read()
-        json_file.close()
-        loaded_model = model_from_json(loaded_model_json)
-
-        #load weights into new model
-        loaded_model.load_weights('Trained_networks/' + seq_lengths['name'][i] + '_weights.h5')
+        # Not all families in sequence_lengths are in Trained_networks
         
-        #new_encoder = loaded_model.layers[1]
-        #if int(loaded_model.layers[0].input_shape[1]/21) < len(test_seq):
-        #    continue
-
-        # add left and right gaps to get the same size sequence as the sequences used for training this network
-        left_gaps = int((int(seq_lengths['size'][i]) - len(test_seq))/2)
-        right_gaps = int(seq_lengths['size'][i]) - len(test_seq) - left_gaps
-        test_seq = (left_gaps*'-') + test_seq + (right_gaps*'-')
-        seq_length = len(test_seq)
-
-        # use the one hot encoded version of the protein sequence
-        single_msa_seq = [test_seq]
-        x_test = to_one_hot(single_msa_seq)
-        y_test = K.argmax(x_test, axis=-1)
-        x_test = x_test.reshape((len(x_test), np.prod(x_test.shape[1:])))
-
-        # reconstruct the new protein sequence with the network
-        a = loaded_model.predict(x_test, steps = 1)
-        a = a.reshape(len(single_msa_seq),seq_length,21)
+        if Path('Trained_networks/' + seq_lengths['name'][i] + '.json').exists():
         
-        # x is the reconstructed sequence
-        x = np.vectorize(get_AA)(np.argmax(a, axis=-1))
-        x = np.array(x).tolist()
-        for k in range(0, len(x)):
-            x[k] = ''.join(x[k])
-        count = 0
-        
-        # find the reconstruction accuracy
-        for j in range(0, len(x[0])):
-            if x[0][j] == single_msa_seq[0][j]:
-                count = count + 1
-        acc = count / len(x[0])
-        
-        # update the max accuracy and the chosen family name
-        if acc > max_acc:
-            max_acc = acc
-            chosen_family = seq_lengths['name'][i]
+            # load the trained network
+            json_file = open('Trained_networks/' + seq_lengths['name'][i] + '.json', 'r')
+            loaded_model_json = json_file.read()
+            json_file.close()
+            loaded_model = model_from_json(loaded_model_json)
+
+            #load weights into new model
+            loaded_model.load_weights('Trained_networks/' + seq_lengths['name'][i] + '_weights.h5')
+            
+            #new_encoder = loaded_model.layers[1]
+            #if int(loaded_model.layers[0].input_shape[1]/21) < len(test_seq):
+            #    continue
+
+            # add left and right gaps to get the same size sequence as the sequences used for training this network
+            left_gaps = int((int(seq_lengths['size'][i]) - len(test_seq))/2)
+            right_gaps = int(seq_lengths['size'][i]) - len(test_seq) - left_gaps
+            test_seq = (left_gaps*'-') + test_seq + (right_gaps*'-')
+            seq_length = len(test_seq)
+
+            # use the one hot encoded version of the protein sequence
+            single_msa_seq = [test_seq]
+            x_test = to_one_hot(single_msa_seq)
+            y_test = K.argmax(x_test, axis=-1)
+            x_test = x_test.reshape((len(x_test), np.prod(x_test.shape[1:])))
+
+            # reconstruct the new protein sequence with the network
+            a = loaded_model.predict(x_test, steps = 1)
+            a = a.reshape(len(single_msa_seq),seq_length,21)
+            
+            # x is the reconstructed sequence
+            x = np.vectorize(get_AA)(np.argmax(a, axis=-1))
+            x = np.array(x).tolist()
+            for k in range(0, len(x)):
+                x[k] = ''.join(x[k])
+            count = 0
+            
+            # find the reconstruction accuracy
+            for j in range(0, len(x[0])):
+                if x[0][j] == single_msa_seq[0][j]:
+                    count = count + 1
+            acc = count / len(x[0])
+            
+            # update the max accuracy and the chosen family name
+            if acc > max_acc:
+                max_acc = acc
+                chosen_family = seq_lengths['name'][i]
     print('The closest protein family is ' + chosen_family + ' with average accuracy of ' + str(max_acc))
     return
 
