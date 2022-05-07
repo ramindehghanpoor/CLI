b4759831ac67d94a48eedceb30ebc708c53554c5
CJ Fogel <fogelcj@gmail.com>
2022-05-07 11:51:30 -0400

    move new sequence search to new_sequence.py, only install dependencies if needed, disable -ns if it isn't supported

--- CLI/__init__.py
+++ CLI/__init__.py
@@ -1,1 +1,4 @@
-import silence_tensorflow.auto
+import sys
+
+if sys.version_info[:3] == (3, 7, 6):
+    import silence_tensorflow.auto
---
+++ CLI/new_sequence.py
@@ -0,0 +1,102 @@
+import numpy as np
+import glob
+import urllib.request
+from shutil import unpack_archive
+from pathlib import Path
+from keras.models import model_from_json
+from .utils import aa_letters
+from .utils.data_loaders import to_one_hot
+from keras import backend as K
+import pandas as pd
+import importlib_resources #new_sequence only called w/ python 3.7.6
+
+def ns_search(ns):
+    
+    if (not Path('Trained_networks').exists()):
+        urllib.request.urlretrieve('https://github.com/cfogel/Trained_networks/releases/download/Trained_networks/Trained_networks.zip', 'downloaded_file.zip')
+        unpack_archive('downloaded_file.zip', 'Trained_networks')
+        os.remove('downloaded_file.zip')
+
+    #
+    # Currently needs to be run from folder with Trained_networks directory in it
+    #
+    
+    protein_seq_file = open(ns,"r+")
+    protein_seq = protein_seq_file.read()
+    
+    # list of all the trained networks. Each trained network belongs to a specific family
+    networks_list = glob.glob('Trained_networks/*.h5')
+
+    # get Amino Acid letter from the one hot encoded version of it
+    def get_AA(n):
+        return list(aa_key.keys())[list(aa_key.values()).index(n)]
+    aa_key = {l: i for i, l in enumerate(aa_letters)}
+
+    max_acc = 0
+    chosen_family = 'none'
+
+    # get the sequence length for each protein family that we have
+    pkg = importlib_resources.files("CLI")
+    f = pkg / "seq_lengths.csv"
+    seq_lengths = pd.read_csv(f,usecols=['name', 'size'])
+
+    # loop over all the trained networks and find the one with highest reconstruction accuracy
+    for i in range(0, len(seq_lengths)):
+        test_seq = protein_seq
+        
+        # skip the families with shorter protein sequence length
+        if int(seq_lengths['size'][i]) < len(test_seq):
+            continue
+
+        # Not all families in sequence_lengths are in Trained_networks
+        
+        if Path('Trained_networks/' + seq_lengths['name'][i] + '.json').exists():
+        
+            # load the trained network
+            json_file = open('Trained_networks/' + seq_lengths['name'][i] + '.json', 'r')
+            loaded_model_json = json_file.read()
+            json_file.close()
+            loaded_model = model_from_json(loaded_model_json)
+
+            #load weights into new model
+            loaded_model.load_weights('Trained_networks/' + seq_lengths['name'][i] + '_weights.h5')
+            
+            #new_encoder = loaded_model.layers[1]
+            #if int(loaded_model.layers[0].input_shape[1]/21) < len(test_seq):
+            #    continue
+
+            # add left and right gaps to get the same size sequence as the sequences used for training this network
+            left_gaps = int((int(seq_lengths['size'][i]) - len(test_seq))/2)
+            right_gaps = int(seq_lengths['size'][i]) - len(test_seq) - left_gaps
+            test_seq = (left_gaps*'-') + test_seq + (right_gaps*'-')
+            seq_length = len(test_seq)
+
+            # use the one hot encoded version of the protein sequence
+            single_msa_seq = [test_seq]
+            x_test = to_one_hot(single_msa_seq)
+            y_test = K.argmax(x_test, axis=-1)
+            x_test = x_test.reshape((len(x_test), np.prod(x_test.shape[1:])))
+
+            # reconstruct the new protein sequence with the network
+            a = loaded_model.predict(x_test, steps = 1)
+            a = a.reshape(len(single_msa_seq),seq_length,21)
+            
+            # x is the reconstructed sequence
+            x = np.vectorize(get_AA)(np.argmax(a, axis=-1))
+            x = np.array(x).tolist()
+            for k in range(0, len(x)):
+                x[k] = ''.join(x[k])
+            count = 0
+            
+            # find the reconstruction accuracy
+            for j in range(0, len(x[0])):
+                if x[0][j] == single_msa_seq[0][j]:
+                    count = count + 1
+            acc = count / len(x[0])
+            
+            # update the max accuracy and the chosen family name
+            if acc > max_acc:
+                max_acc = acc
+                chosen_family = seq_lengths['name'][i]
+    print('The closest protein family is ' + chosen_family + ' with average accuracy of ' + str(max_acc))
+    return
