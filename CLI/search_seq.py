481e26391b58ca4e487376d674900afb891062c2
CJ Fogel <fogelcj@gmail.com>
2022-05-18 21:10:00 -0400

    solve h5py issue

--- CLI/search_seq.py
+++ CLI/search_seq.py
@@ -3,21 +3,16 @@ from .output_results import output_result
 
 
 def seq_search(args):
-    if sys.version == '3.7.6 (default, Jan  8 2020, 19:59:22) \n[GCC 7.3.0]':
+    if sys.version_info[:2] == (3, 7) and ('32 bit' not in sys.version):
         # don't import function and dependencies if you don't need to
-        from .SearchSQ import SearchSQ
-        for ns in args.sequence:
-            try:
+        try:
+            from .SearchSQ import SearchSQ
+            for ns in args.sequence:
                 res = SearchSQ(ns).result
-            except Exception as err:
-                print("Error")
-                exit(err)
-            else:
                 output_result(res, args.output_file, args.output_format, args.output_mode)
+        except Exception as err:
+            print("Error")
+            exit(err)
 
     else:
-        errormessage = "Sequence searches not supported on this python version."
-        url = "https://github.com/cfogel/CLI/raw/main/compbiolab-cli.yml"
-        dowloadmessage = "See " + url + " for the system requirements to use this feature."
-        print(errormessage)
-        print(dowloadmessage)
+        print("Sequence searches only supported on 64-bit Python 3.7")
